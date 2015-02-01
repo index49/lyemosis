c6a6fe1136f7b448d49fff5ef05882b07cd5915f
Vi Nguyen <vi.h.nguyen@gmail.com>
2015-02-01 01:43:04 -0500

    sign in google account

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import httplib2
 import webapp2
 import random
+import math
 import json
 import cgi
 import urllib
@@ -17,13 +18,17 @@ credentials = AppAssertionCredentials(scope=url)
 httpss = credentials.authorize(httplib2.Http())
 bigquery_service = build('bigquery','v2',http=httpss)
 DEFAULT_CIRCLE_DB_NAME = 'default_circle_db'
+DEFAULT_PLAYER_DB_NAME = 'default_player_db'
 
-# returns the circle db?
+# returns the db?
 def circle_db_key(circle_db_name = DEFAULT_CIRCLE_DB_NAME):
 	return ndb.Key('CircleDB', circle_db_name)
+def player_db_key(player_db_name = DEFAULT_PLAYER_DB_NAME):
+	return ndb.Key('PlayerDB', player_db_name)
 
 class Player(ndb.Model):
 	name = ndb.StringProperty(indexed = False)
+	color = ndb.StringProperty(indexed = False)
 
 class Circle(ndb.Model):
 	player = ndb.StructuredProperty(Player)
@@ -42,20 +47,35 @@ class PollCircleData(webapp2.RequestHandler):
 # AddCircle
 #	http://localhost:8080/addCircle?x=200&y=100&name=usrname
 class AddCircle(webapp2.RequestHandler):
+
 	def post(self):
 
 		# get db name 
 		circle_db_name = self.request.get('circle_db_name', DEFAULT_CIRCLE_DB_NAME)
+		circles = Circle.query(ancestor=circle_db_key(DEFAULT_CIRCLE_DB_NAME)).order(Circle.date)
 
 		# create item to add
 		circle = Circle(parent=circle_db_key(circle_db_name))
 		circle.player = Player(
-			name = self.request.get('name'))#name = users.get_current_user().user_id()
+			name = self.request.get('name'), color = 'blue')
 		circle.x = int(self.request.get('x'))
 		circle.y = int(self.request.get('y'))
 
+		# check if circle already exists
+		radius = 10 #pixels
+		addFlag = True
+		for c in circles:
+			dx = c.x - circle.x
+			dy = c.y - circle.y
+			d = math.sqrt(dx*dx + dy*dy)
+			if d < radius:
+				addFlag = False
+				c.key.delete()
+				break
+
 		# add to db
-		circle.put()
+		if addFlag is True:
+			circle.put()
 
 		# redirect to get data page
 		#self.redirect('/displayChart?')
@@ -70,7 +90,7 @@ class GetCircleData(webapp2.RequestHandler):
 		resp = []
 		circles = Circle.query(ancestor=circle_db_key(DEFAULT_CIRCLE_DB_NAME)).order(Circle.date)
 		for c in circles:
-			resp.append({ 'x': c.x, 'y': c.y })
+			resp.append({ 'x': c.x, 'y': c.y , 'c': c.player.color})
 
 		# send circle data back as json
 		self.response.headers['Content-Type'] = 'application/json'
@@ -97,6 +117,14 @@ class DisplayChart(webapp2.RequestHandler):
 		temp_data = {}
 		temp_path = 'Templates/displayChart.html'
 		self.response.out.write(template.render(temp_path, temp_data))
+
+		#TODO
+		#if users.get_current_user():
+		#	print users.get_current_user().user_id()
+		#	#print users.get_current_user().email()
+		#else:
+		#	self.redirect(users.create_login_url(self.request.uri))
+
 
 class GetChartData(webapp2.RequestHandler):
 	def get(self):
